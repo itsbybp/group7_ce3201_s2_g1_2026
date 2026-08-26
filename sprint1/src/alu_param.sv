b5bb57b8b69aa7cb826bd86182fd2de5e649f303
Byron BP <byronbp1306@outlook.com>
2026-08-25 18:33:26 -0600

    feat: implement US 1.3 ALU status flags

--- sprint1/src/alu_param.sv
+++ sprint1/src/alu_param.sv
@@ -3,7 +3,8 @@ module alu_param  #(parameter WIDTH = 8)
     input alu_types_pkg::alu_op_t code,
     input logic [WIDTH - 1:0] a,
     input logic [WIDTH - 1:0] b,
-    output logic [WIDTH - 1:0] result
+    output logic [WIDTH - 1:0] result,
+    output alu_types_pkg::alu_flags_t flags
 );
 
   import alu_types_pkg::*;
@@ -12,15 +13,19 @@ module alu_param  #(parameter WIDTH = 8)
 
     logic [WIDTH - 1:0] b_mod;
     logic [WIDTH - 1:0] add_one;
+    logic [WIDTH:0] full_res;
     b_mod = '0;
     add_one = '0;
+    full_res = '0;
+    flags = '0;
 
     case (code)
 
       ADD, SUB: begin
         b_mod = (code == ADD) ? b : ~b;
         add_one[0] = (code == SUB);
-        result = a + b_mod + add_one;
+        full_res = {1'b0, a} + {1'b0, b_mod} + {1'b0, add_one};
+        result = full_res[WIDTH - 1:0];
       end
 
       AND:
@@ -43,8 +48,14 @@ module alu_param  #(parameter WIDTH = 8)
 
       default:
         result = '0;
-
     endcase
+
+    flags.zero = (result == '0);
+    flags.negative = result[WIDTH - 1];
+    flags.carry = full_res[WIDTH];
+    flags.overflow = ((code == ADD) && (a[WIDTH-1] == b[WIDTH-1]) && (result[WIDTH-1] != a[WIDTH-1]))
+    || ((code == SUB) && (a[WIDTH-1] != b[WIDTH-1]) && (result[WIDTH-1] != a[WIDTH-1]));
+
   end
 
 
